94b9bd5d16a58a212348de27e425cb56272f0be8
dtregonning <dtregonning@esplunk.com>
2019-02-15 09:39:16 -0800

    WIP

--- test/helper.rb
+++ test/helper.rb
@@ -3,22 +3,40 @@ require 'fluent/plugin/in_kubernetes_metrics_aggregator.rb'
 SimpleCov.start
 
 $LOAD_PATH.unshift(File.expand_path("../../", __FILE__))
-require "test-unit"
-require "fluent/test"
-require "fluent/test/driver/input"
-require "fluent/test/helpers"
+require 'test-unit'
+require 'fluent/test'
+require 'fluent/test/driver/input'
+require 'fluent/test/helpers'
 require 'webmock/test_unit'
 
 Test::Unit::TestCase.include(Fluent::Test::Helpers)
 Test::Unit::TestCase.extend(Fluent::Test::Helpers)
 
-class MetricsHelper
-  def create_driver(conf)
-    @@driver = Fluent::Test::Driver::Input.new(Fluent::Plugin::KubernetesMetricsAggregatorInput).configure(conf)
-    #return @@driver
+module PluginTestHelper
+
+  def k8s_host
+    'apiserver'
   end
 
-  def stub_api_10250
+  def k8s_port
+     '10255'
+  end
+
+  def k8s_url(path='api')
+    "https://#{k8s_host}:#{k8s_port}/#{path}"
+  end
+
+  def kubelet_summary_api_url_node1
+    k8s_url + '/v1/nodes/generics-aws-node-one:10255/proxy/stats/summary'
+  end
+  def kubelet_summary_api_url_node2
+    k8s_url + '/v1/nodes/generics-aws-node-two:10255/proxy/stats/summary'
+  end
+  def kubelet_summary_api_url_node3
+    k8s_url + '/v1/nodes/generics-aws-node-three:10255/proxy/stats/summary'
+  end
+
+  def stub_api_port_10250
     WebMock.stub_request(:get, "https://localhost:10250/api").
         with(
             headers: {
@@ -29,7 +47,7 @@ class MetricsHelper
         to_return(status: 200, body: open(File.expand_path('../v1.json', __FILE__)) , headers: {})
   end
 
-  def stub_api_10255
+  def stub_api_port_10255
     WebMock.stub_request(:get, "https://node.fakedestination.com:10255/api").
         with(
             headers: {
@@ -38,5 +56,65 @@ class MetricsHelper
                 'Host'=>'node.fakedestination.com:10255'
             }).
         to_return(status: 200, body: open(File.expand_path('../v1.json', __FILE__)) , headers: {})
+  end
+
+  def stub_k8s_v1
+    File.open(File.expand_path('v1.json', __dir__)).tap do |f|
+      stub_request(:get, "#{k8s_url}/v1")
+        .to_return(body: f.read)
+    end
+  end
+
+  def stub_k8s_api
+    File.open(File.expand_path('api.json', __dir__)).tap do |f|
+      stub_request(:get, k8s_url)
+        .to_return(body: f.read)
+    end
+  end
+
+  def stub_k8s_api_pods
+    File.open(File.expand_path('pods.json', __dir__)).tap do |f|
+      stub_request(:get, k8s_url + '/v1/pods')
+        .to_return(body: f.read)
+    end
+  end
+
+  def stub_k8s_api_nodes
+    File.open(File.expand_path('/nodes.json', __dir__)).tap do |f|
+      stub_request(:get, k8s_url + '/v1/nodes')
+        .to_return(body: f.read)
+    end
+  end
+
+  def stub_kubelet_summary_api_node1
+    File.open(File.expand_path('node1.json', __dir__)).tap do |f|
+      stub_request(:get, kubelet_summary_api_url_node1.to_s)
+        .to_return(body: f.read)
+    end
+  end
+
+  def stub_kubelet_summary_api_node2
+    File.open(File.expand_path('node2.json', __dir__)).tap do |f|
+      stub_request(:get, kubelet_summary_api_url_node2.to_s)
+        .to_return(body: f.read)
+    end
+  end
+
+  def stub_kubelet_summary_api_node3
+    File.open(File.expand_path('node3.json', __dir__)).tap do |f|
+      stub_request(:get, kubelet_summary_api_url_node3.to_s)
+        .to_return(body: f.read)
+    end
+  end
+
+  def stub_k8s_requests
+    stub_api_port_10250
+    stub_api_port_10255
+    stub_k8s_api
+    stub_k8s_api_pods
+    stub_k8s_api_nodes
+    stub_kubelet_summary_api_node1
+    stub_kubelet_summary_api_node2
+    stub_kubelet_summary_api_node3
   end
 end
--- test/plugin/test_in_kubernetes_metrics_aggregator.rb
+++ test/plugin/test_in_kubernetes_metrics_aggregator.rb
@@ -3,50 +3,56 @@ require 'fluent/plugin/in_kubernetes_metrics_aggregator.rb'
 
 class KubernetesMetricsAggInputTest < Test::Unit::TestCase
   include Fluent::Test::Helpers
-  @@metricsHelper = MetricsHelper.new
+  include PluginTestHelper
 
-  def setup
-    Fluent::Test.setup
-    @@metricsHelper.create_driver(ZERO_CONFIG)
+  @driver = nil
+  @hash_map_test = {}
+  @hash_map_cadvisor = {}
 
-  end
+  ZERO_CONFIG = %([]).freeze
 
-  def teardown
-
-  end
-
-  @@metricsHelper.stub_api_10250
-  @@metricsHelper.stub_api_10255
-
-  ZERO_CONFIG = %[]
-
-  BASIC_CONFIG = %[
+  BASIC_CONFIG = %([
       kubernetes_url https://node.fakedestination.com
       kubelet_port 10255
-  ]
+  ]).freeze
 
-  ADVANCED_CONFIG_NO_CERTS = %[
+  ADVANCED_CONFIG_NO_CERTS = %([
       kubernetes_url https://node.fakedestination.com
       kubelet_port 10255
       tag test.tag.check
       interval 2m
       insecure_ssl true
       cluster_name awesome_cluster
+  ]).freeze
 
-  ]
-
-  ADVANCED_CONFIG_CERTS = %[
+  ADVANCED_CONFIG_CERTS = %([
       kubernetes_url https://node.fakedestination.com
       kubelet_port 10255
+  ]).freeze
 
-  ]
+  setup do
+    Fluent::Test.setup
+    stub_k8s_requests
+    @driver = create_driver
+    @driver.run timeout: 20, expect_emits: 1, shutdown: true
+
+    @driver.events.each do |tag, time, record|
+      @hash_map_test[tag] = tag, time, record
+    end
+  end
+
+  def create_driver(conf = BASIC_CONFIG)
+    Fluent::Test::Driver::Input
+      .new(Fluent::Plugin::KubernetesMetricsAggregatorInput)
+      .configure(conf)
+  end
 
   sub_test_case 'default parameter configuration' do
     test 'test default params' do
-      d = @@metricsHelper.create_driver(ZERO_CONFIG)
+      d = create_driver(ZERO_CONFIG)
       assert_equal 'https://localhost', d.instance.kubernetes_url
-      assert_equal 10250 , d.instance.kubelet_port
-      assert_equal 'kubernetes.metrics.*',  d.instance.tag
+      assert_equal 10_250, d.instance.kubelet_port
+      assert_equal 'kubernetes.metrics.*', d.instance.tag
       assert_equal '15s', d.instance.interval
       assert_nil d.instance.kubeconfig
       assert_nil d.instance.client_cert
@@ -54,34 +60,31 @@ class KubernetesMetricsAggInputTest < Test::Unit::TestCase
       assert_nil d.instance.ca_file
       assert_false d.instance.insecure_ssl
       assert_nil d.instance.bearer_token_file
-      assert_equal '/var/run/secrets/kubernetes.io/serviceaccount', d.instance.secret_dir
+      assert_equal '/var/run/secrets/kubernetes.io/serviceaccount',
+                   d.instance.secret_dir
       assert_equal 'cluster_name', d.instance.cluster_name
     end
   end
 
   sub_test_case 'modify parameter changes' do
     test 'test kubelet_port and supplied kubernetes URL parameters' do
-      d = @@metricsHelper.create_driver(BASIC_CONFIG)
+      d = create_driver(BASIC_CONFIG)
       assert_equal 'https://node.fakedestination.com', d.instance.kubernetes_url
-      assert_equal 10255 , d.instance.kubelet_port
+      assert_equal 10_255, d.instance.kubelet_port
     end
 
     test 'test tag and interval parameters' do
-      d = @@metricsHelper.create_driver(ADVANCED_CONFIG_NO_CERTS)
+      d = create_driver(ADVANCED_CONFIG_NO_CERTS)
       assert_equal 'test.tag.check', d.instance.tag
-      assert_equal 120 , d.instance.interval
+      assert_equal 120, d.instance.interval
     end
 
     test 'test insecure_ssl and cluster_name parameters ' do
-     d = @@metricsHelper.create_driver(ADVANCED_CONFIG_NO_CERTS)
-     assert_true d.instance.insecure_ssl
-     assert_equal 'awesome_cluster' , d.instance.cluster_name
+      d = create_driver(ADVANCED_CONFIG_NO_CERTS)
+      assert_true d.instance.insecure_ssl
+      assert_equal 'awesome_cluster', d.instance.cluster_name
     end
   end
 
-  #TODO:ADD Certificate and Bearer token tests.
-
-
-
-
+  # TODO: ADD Certificate and Bearer token tests.
 end
