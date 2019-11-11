c5d2f23c4d1578bcbb7e229dc0d903bd6ca7306d
Rock Baek <rbaek@splunk.com>
2019-11-11 10:09:38 -0800

    fix image name

--- docker/build.sh
+++ docker/build.sh
@@ -13,4 +13,4 @@ cp pkg/fluent-plugin-*.gem docker
 
 # Build Docker Image
 FLUENTD_HEC_GEM_VERSION=`cat docker/FLUENTD_HEC_GEM_VERSION`
-docker build --build-arg VERSION=$FLUENTD_HEC_GEM_VERSION --no-cache -t splunk/k8s-metrics:$TAG ./docker
+docker build --build-arg VERSION=$FLUENTD_HEC_GEM_VERSION --no-cache -t splunk/k8s-metrics-aggr:$TAG ./docker
