b2838e34947dde74ccfd30fbef385fc69509d5ca
Chaitanya Phalak <cphalak@splunk.com>
2019-12-11 13:12:36 -0800

    Add licenses to local build script

--- docker/build.sh
+++ docker/build.sh
@@ -13,4 +13,7 @@ cp pkg/fluent-plugin-*.gem docker
 
 # Build Docker Image
 FLUENTD_HEC_GEM_VERSION=`cat docker/FLUENTD_HEC_GEM_VERSION`
+echo "Copying licenses to be included in the docker image..."
+mkdir licenses
+cp -rp LICENSE licenses/
 docker build --build-arg VERSION=$FLUENTD_HEC_GEM_VERSION --no-cache -t splunk/k8s-metrics-aggr:$TAG ./docker
